a9a8b659f66b31279d03f894b23cc9dc67b028e4
gregatemi <greg.atemi@systechafrica.com>
2025-08-14 23:29:38 +0300

    Added hero image

---
+++ src/components/HeroImage.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './HeroImage.css'
+
+function HeroImage() {
+
+  return (
+    <>
+      <div className='HeroImage'>
+        <p>Kenya Judo Federation</p>
+      </div>
+    </>
+  )
+}
+
+export default HeroImage
