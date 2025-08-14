844e204f6816dca1d0bd08da802bd400a077dcba
gregatemi <greg.atemi@systechafrica.com>
2025-08-14 23:28:58 +0300

    Added hero image component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar.jsx'
+import HeroImage from './components/HeroImage.jsx'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <HeroImage />
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,25 @@
 import { useState } from 'react'
 import './Navbar.css'
 
-function App() {
+function Navbar() {
 
   return (
     <>
-      <ul className="topnav">
-        <li className="logo"><div className="Logo-placeholder" /></li>
-        <li><a className="active" href="#home">Home</a></li>
-        <li><a href="#events">Events</a></li>
-        <li><a href="#clubs">Clubs</a></li>
-        <li><a href="#fans">Fans</a></li>
-        <li><a href="#gallery">Gallery</a></li>
-        <li><a href="#ranking">Ranking</a></li>
-        <li><a href="#about">About Us</a></li>
-        <li><a href="#contact">Contact Us</a></li>
-    </ul>
+      <div>
+        <ul className="topnav">
+          <li className="logo"><div className="Logo-placeholder" /></li>
+          <li><a className="active" href="#home">Home</a></li>
+          <li><a href="#events">Events</a></li>
+          <li><a href="#clubs">Clubs</a></li>
+          <li><a href="#fans">Fans</a></li>
+          <li><a href="#gallery">Gallery</a></li>
+          <li><a href="#ranking">Ranking</a></li>
+          <li><a href="#about">About Us</a></li>
+          <li><a href="#contact">Contact Us</a></li>
+        </ul>
+      </div>
     </>
   )
 }
 
-export default App
+export default Navbar
