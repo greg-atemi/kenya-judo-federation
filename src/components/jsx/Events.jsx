a3911812f1cbb1f64bb4e28d964016b7c7789725
gregatemi <greg.atemi@systechafrica.com>
2025-08-16 22:14:20 +0300

    Created and styled events component

---
+++ src/components/jsx/Events.jsx
@@ -0,0 +1,88 @@
+import '../styles/Events.css';
+
+function Events() {
+
+  return (
+    <>
+      <section>
+        <h1>Upcoming Events</h1>
+        <div className='EventListGroup'>
+          <div>
+            <p className='EventTitle'>SENIORS</p>
+            <div className='EventList'>
+              <div className='Event'>
+                <div className='EventDate'>
+                  <p>19</p>
+                  <p>JAN</p>
+                </div>
+                <div className='EventName'>
+                  <p>Kiambu Champoinship</p>
+                </div>
+              </div>
+              <div className='Event'>
+                <div className='EventDate'>
+                  <p>19</p>
+                  <p>JAN</p>
+                </div>
+                <div className='EventName'>
+                  <p>Kiambu Champoinship</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <p className='EventTitle'>SENIORS</p>
+            <div className='EventList'>
+              <div className='Event'>
+                <div className='EventDate'>
+                  <p>19</p>
+                  <p>JAN</p>
+                </div>
+                <div className='EventName'>
+                  <p>Kiambu Champoinship</p>
+                </div>
+              </div>
+              <div className='Event'>
+                <div className='EventDate'>
+                  <p>19</p>
+                  <p>JAN</p>
+                </div>
+                <div className='EventName'>
+                  <p>Kiambu Champoinship</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <p className='EventTitle'>SENIORS</p>
+            <div className='EventList'>
+              <div className='Event'>
+                <div className='EventDate'>
+                  <p>19</p>
+                  <p>JAN</p>
+                </div>
+                <div className='EventName'>
+                  <p>Kiambu Champoinship</p>
+                </div>
+              </div>
+              <div className='Event'>
+                <div className='EventDate'>
+                  <p>19</p>
+                  <p>JAN</p>
+                </div>
+                <div className='EventName'>
+                  <p>Kiambu Champoinship</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Events
