c88d5ffdc603455fa40bdb897e552963cf91786e
gregatemi <greg.atemi@systechafrica.com>
2025-08-16 19:13:31 +0300

    Changed project structure

---
+++ src/components/jsx/HeroImage.jsx
@@ -0,0 +1,14 @@
+import '../styles/HeroImage.css';
+
+function HeroImage() {
+
+  return (
+    <>
+      <div className='HeroImage'>
+        <p>Kenya Judo Federation</p>
+      </div>
+    </>
+  )
+}
+
+export default HeroImage
