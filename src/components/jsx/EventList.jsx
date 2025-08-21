fc981afc8d267c7e75ec85e15dbd663133f7ea63
gregatemi <greg.atemi@systechafrica.com>
2025-08-21 18:29:33 +0300

    Added Event list page structure

--- src/components/jsx/EventList.jsx
+++ src/components/jsx/EventList.jsx
@@ -5,7 +5,7 @@ function EventList() {
   return (
     <>
       <div>
-      <div className='EventFilterGroup'>
+        <div className='EventFilterGroup'>
           <div className='EventFilter'>
             <p>Region</p>
             <select>
@@ -14,16 +14,35 @@ function EventList() {
               <option value="central">Central</option>
               <option value="eastern">Eastern</option>
             </select>
+            <div>
+              <p>Latest Competition</p>
+              <p>Kiambu National Championships 2025</p>
+              <p>Date 23/08/2025</p>
+            </div>
           </div>
 
           <div className='EventFilter'>
-            <p>Region</p>
+            <p>Year</p>
             <select>
-              <option value="select">Select Event</option>
-              <option value="nairobi">Nairobi</option>
-              <option value="central">Central</option>
-              <option value="eastern">Eastern</option>
+              <option value="select">Select Year</option>
+              <option value="2025">2025</option>
+              <option value="2024">2024</option>
+              <option value="2023">2023</option>
             </select>
+            <div>
+              <p>Next Competition</p>
+              <p>Kiambu National Championships 2025</p>
+              <p>Date 23/08/2025</p>
+            </div>
+          </div>
+        </div>
+
+        <div className='EventContainer'>
+          <div>
+            <span>Kiambu National Championships 2025</span>
+            <span>Date: 23/08/2025</span>
+            <span>Location: Kiambu</span>
+            <span>Description: Annual athletics competition featuring top athletes from the region.</span>
           </div>
         </div>
       </div>
