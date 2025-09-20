f591d34c17619ab5e8ff88e913a94a5ba4b91410
gregatemi <greg.atemi@systechafrica.com>
2025-09-20 18:43:19 +0300

    Added colouring to navbar

--- src/components/jsx/Navbar.jsx
+++ src/components/jsx/Navbar.jsx
@@ -10,74 +10,47 @@ function Navbar() {
               <div className="Logo-placeholder"></div>
           </li>
           <li>
-            <NavLink 
-              to="/" 
-              className={({ isActive }) => isActive ? "active" : ""}
-            >
+            <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink 
-              to="/events" 
-              className={({ isActive }) => isActive ? "active" : ""}
-            >
+            <NavLink to="/events" className={({ isActive }) => isActive ? "active" : ""}>
               Events
             </NavLink>
           </li>
           <li>
-            <NavLink 
-              to="/clubs" 
-              className={({ isActive }) => isActive ? "active" : ""}
-            >
+            <NavLink to="/clubs" className={({ isActive }) => isActive ? "active" : ""}>
               Clubs
             </NavLink>
           </li>
           <li>
-            <NavLink 
-              to="/fans" 
-              className={({ isActive }) => isActive ? "active" : ""}
-            >
+            <NavLink to="/fans" className={({ isActive }) => isActive ? "active" : ""}>
               Fans
             </NavLink>
           </li>
           <li>
-            <NavLink 
-              to="/ranking" 
-              className={({ isActive }) => isActive ? "active" : ""}
-            >
+            <NavLink to="/ranking" className={({ isActive }) => isActive ? "active" : ""}>
               Ranking
             </NavLink>
           </li>
           <li>
-            <NavLink 
-              to="/kjf-history" 
-              className={({ isActive }) => isActive ? "active" : ""}
-            >
+            <NavLink to="/kjf-history" className={({ isActive }) => isActive ? "active" : ""}>
               KJF History
             </NavLink>
           </li>
           <li>
-            <NavLink 
-              to="/judo-history" 
-              className={({ isActive }) => isActive ? "active" : ""}
-            >
+            <NavLink to="/judo-history" className={({ isActive }) => isActive ? "active" : ""}>
               Judo History
             </NavLink>
           </li>
           <li>
-            <NavLink 
-              to="/leadership" 
-              className={({ isActive }) => isActive ? "active" : ""}
-            >
+            <NavLink to="/leadership" className={({ isActive }) => isActive ? "active" : ""}>
               Our Leadership
             </NavLink>
           </li>
           <li>
-            <NavLink 
-              to="/contact" 
-              className={({ isActive }) => isActive ? "active" : ""}
-            >
+            <NavLink to="/contact" className={({ isActive }) => isActive ? "active" : ""}>
               Contact Us
             </NavLink>
           </li>
