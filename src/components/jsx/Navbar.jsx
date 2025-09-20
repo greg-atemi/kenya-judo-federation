a1d2a33a8212018701d62fb4635bf12534c7b899
gregatemi <greg.atemi@systechafrica.com>
2025-09-20 19:20:19 +0300

    Added burger button to navabar mobile view

--- src/components/jsx/Navbar.jsx
+++ src/components/jsx/Navbar.jsx
@@ -1,63 +1,44 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import '../styles/Navbar.css';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <>
-      <div>
-        <ul className="topnav">
-          <li className="logo">
-              <div className="Logo-placeholder"></div>
-          </li>
+      <div className="navbar">
+        <div className="logo">
+          <div className="Logo-placeholder"></div>
+        </div>
+
+        {/* Burger button */}
+        <div className={`burger ${isOpen ? "open" : ""}`} onClick={toggleMenu}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+
+        <ul className={`topnav ${isOpen ? "open" : ""}`}>
           <li>
             <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>
               Home
             </NavLink>
           </li>
-          <li>
-            <NavLink to="/events" className={({ isActive }) => isActive ? "active" : ""}>
-              Events
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/clubs" className={({ isActive }) => isActive ? "active" : ""}>
-              Clubs
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/fans" className={({ isActive }) => isActive ? "active" : ""}>
-              Fans
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/ranking" className={({ isActive }) => isActive ? "active" : ""}>
-              Ranking
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/kjf-history" className={({ isActive }) => isActive ? "active" : ""}>
-              KJF History
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/judo-history" className={({ isActive }) => isActive ? "active" : ""}>
-              Judo History
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/leadership" className={({ isActive }) => isActive ? "active" : ""}>
-              Our Leadership
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact" className={({ isActive }) => isActive ? "active" : ""}>
-              Contact Us
-            </NavLink>
-          </li>
+          <li><NavLink to="/events">Events</NavLink></li>
+          <li><NavLink to="/clubs">Clubs</NavLink></li>
+          <li><NavLink to="/fans">Fans</NavLink></li>
+          <li><NavLink to="/ranking">Ranking</NavLink></li>
+          <li><NavLink to="/kjf-history">KJF History</NavLink></li>
+          <li><NavLink to="/judo-history">Judo History</NavLink></li>
+          <li><NavLink to="/leadership">Our Leadership</NavLink></li>
+          <li><NavLink to="/contact">Contact Us</NavLink></li>
         </ul>
       </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
