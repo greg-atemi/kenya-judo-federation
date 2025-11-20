d8094722337b34cf8f3dfe0795cb0c2e6177c77f
gregatemi <greg.atemi@systechafrica.com>
2025-11-20 19:44:24 +0300

    Added border shadow to events list page

--- src/components/jsx/Events.jsx
+++ src/components/jsx/Events.jsx
@@ -3,83 +3,108 @@ import '../styles/Events.css';
 import eventsData from '../../../data/events.json'; // Import the JSON file
 
 function Events() {
-
   return (
     <>
-      <HeroImage/>
+      <HeroImage />
       <section className='Section'>
         <h1>Upcoming Events</h1>
         <div className='EventListGroup'>
+          {/* Seniors Section */}
           <div>
             <p className='EventTitle'>SENIORS</p>
             <div className='EventList'>
               <div className='Event'>
                 <div className='EventDate'>
-                  <p>19 JAN</p>
+                  <p>{eventsData.Seniors[0].date}</p>
                 </div>
                 <div className='EventName'>
-                  <p>Kiambu Championship</p>
+                  <p>{eventsData.Seniors[0].event}</p>
                 </div>
               </div>
               <div className='Event'>
                 <div className='EventDate'>
-                  <p>19 JAN</p>
+                  <p>{eventsData.Seniors[1].date}</p>
                 </div>
                 <div className='EventName'>
-                  <p>Kiambu Championship</p>
+                  <p>{eventsData.Seniors[1].event}</p>
+                </div>
+              </div>
+              <div className='Event'>
+                <div className='EventDate'>
+                  <p>{eventsData.Seniors[2].date}</p>
+                </div>
+                <div className='EventName'>
+                  <p>{eventsData.Seniors[2].event}</p>
                 </div>
               </div>
             </div>
           </div>
 
+          {/* Juniors Section */}
           <div>
-            <p className='EventTitle'>SENIORS</p>
+            <p className='EventTitle'>JUNIORS</p>
             <div className='EventList'>
               <div className='Event'>
                 <div className='EventDate'>
-                  <p>19 JAN</p>
+                  <p>{eventsData.Juniors[0].date}</p>
                 </div>
                 <div className='EventName'>
-                  <p>Kiambu Championship</p>
+                  <p>{eventsData.Juniors[0].event}</p>
                 </div>
               </div>
               <div className='Event'>
                 <div className='EventDate'>
-                  <p>19 JAN</p>
+                  <p>{eventsData.Juniors[1].date}</p>
                 </div>
                 <div className='EventName'>
-                  <p>Kiambu Championship</p>
+                  <p>{eventsData.Juniors[1].event}</p>
+                </div>
+              </div>
+              <div className='Event'>
+                <div className='EventDate'>
+                  <p>{eventsData.Juniors[2].date}</p>
+                </div>
+                <div className='EventName'>
+                  <p>{eventsData.Juniors[2].event}</p>
                 </div>
               </div>
             </div>
           </div>
 
+          {/* Cadets Section */}
           <div>
-            <p className='EventTitle'>SENIORS</p>
+            <p className='EventTitle'>CADETS</p>
             <div className='EventList'>
               <div className='Event'>
                 <div className='EventDate'>
-                  <p>19 JAN</p>
+                  <p>{eventsData.Cadets[0].date}</p>
                 </div>
                 <div className='EventName'>
-                  <p>Kiambu Championship</p>
+                  <p>{eventsData.Cadets[0].event}</p>
                 </div>
               </div>
               <div className='Event'>
                 <div className='EventDate'>
-                  <p>19 JAN</p>
+                  <p>{eventsData.Cadets[1].date}</p>
                 </div>
                 <div className='EventName'>
-                  <p>Kiambu Championship</p>
+                  <p>{eventsData.Cadets[1].event}</p>
+                </div>
+              </div>
+              <div className='Event'>
+                <div className='EventDate'>
+                  <p>{eventsData.Cadets[2].date}</p>
+                </div>
+                <div className='EventName'>
+                  <p>{eventsData.Cadets[2].event}</p>
                 </div>
               </div>
             </div>
           </div>
-
         </div>
       </section>
     </>
-  )
+  );
 }
 
 export default Events;
