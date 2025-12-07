44c48bbe88d43355f6f17722264c20c2bd83a806
gregatemi <magrega7180@gmail.com>
2025-12-07 14:15:08 +0300

    Added styling to Fans page for Desktop view

--- src/components/jsx/Fans.jsx
+++ src/components/jsx/Fans.jsx
@@ -7,58 +7,62 @@ function Fans() {
     <>
       <HeroImage />
       <div className='fansPageContainer'>
-        <h2>To all our dear Fans...</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc hendrerit ex at augue faucibus, id aliquam magna consequat. Mauris ullamcorper turpis vitae ex tristique, in ultrices urna vestibulum. Pellentesque urna sapien, convallis nec condimentum vitae, ullamcorper ac nibh. Quisque consectetur gravida odio eget sagittis. Nam pharetra est non sapien facilisis, eget placerat nunc blandit. Quisque sollicitudin sem non massa ultrices malesuada. Suspendisse mauris dui, iaculis in felis id, pretium vulputate velit. Nam faucibus facilisis semper. Aliquam eu nisl magna. Morbi finibus ligula id elementum aliquam. Pellentesque id dignissim mi, sed viverra odio. Phasellus a neque a dui dapibus auctor nec a erat. Ut dignissim erat eget posuere fermentum. Nullam malesuada tincidunt nibh lobortis accumsan. Ut vestibulum tincidunt pharetra.
-        </p>
-      </div>
-      <div className='feedbackContainer'>
         <div>
-          <h3>We value your feedback</h3>
-          <p>Contact Us</p>
-          <p>Email, call or submit the form to learn how to...</p>
-          <p>gitothua@yahoo.com</p>
-          <p>+254 123 456908</p>
-          <p>Follow Us</p>
+          <h2>To all our dear Fans...</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc hendrerit ex at augue faucibus, id aliquam magna consequat. Mauris ullamcorper turpis vitae ex tristique, in ultrices urna vestibulum. Pellentesque urna sapien, convallis nec condimentum vitae, ullamcorper ac nibh. Quisque consectetur gravida odio eget sagittis. Nam pharetra est non sapien facilisis, eget placerat nunc blandit. Quisque sollicitudin sem non massa ultrices malesuada. Suspendisse mauris dui, iaculis in felis id, pretium vulputate velit. Nam faucibus facilisis semper. Aliquam eu nisl magna. Morbi finibus ligula id elementum aliquam. Pellentesque id dignissim mi, sed viverra odio. Phasellus a neque a dui dapibus auctor nec a erat. Ut dignissim erat eget posuere fermentum. Nullam malesuada tincidunt nibh lobortis accumsan. Ut vestibulum tincidunt pharetra.
+          </p>
         </div>
-        <div>
-          <form>
-            <div className="firstElementWrapper">
-              <h3>Get in Touch</h3>
-            </div>
-            <div className='fNameLastNameGroup'>
-              <div>
-                <label htmlFor="firstName">First Name</label>
-                <input id="firstName" type="text" />
+        <div className='fansPageContainerContactUs'>
+          <div>
+            <h2>We value your feedback</h2>
+            <p>Contact Us</p>
+            <br></br>
+            <p>Email: gitothua@yahoo.com</p>
+            <br></br>
+            <p>Phone: +254 123 456908</p>
+            <br></br>
+            <p>Follow Us</p>
+          </div>
+          <div>
+            <form>
+              <div className="firstElementWrapper">
+                <h2>Get in Touch</h2>
               </div>
-              <div>
-                <label htmlFor="lastName">Last Name</label>
-                <input id="lastName" type="text" />
+              <div className='fNameLastNameGroup'>
+                <div>
+                  <label htmlFor="firstName">First Name</label>
+                  <input id="firstName" type="text" />
+                </div>
+                <div>
+                  <label htmlFor="lastName">Last Name</label>
+                  <input id="lastName" type="text" />
+                </div>
               </div>
-            </div>
-            <div className='Normal'>
-              <label htmlFor="email">Email</label>
-              <input id="email" type="email" />
-            </div>
-            <div className='Normal'>
-              <label htmlFor="phone">Phone Number</label>
-              <div className='phoneInput'>
-                <select style={{ width: '22%' }} id="code" type="tel">
-                  {countryCodes.map((country, index) => (
-                    <option key={index} value={country.code}>
-                      {country.flag} {country.code}
-                    </option>
-                  ))}
-                </select>
-                <input style={{ width: '78%' }} id="phone" type="tel" />
+              <div className='Normal'>
+                <label htmlFor="email">Email</label>
+                <input id="email" type="email" />
               </div>
-            </div>
-            <div className='Normal'>
-              <label htmlFor="message">Message</label>
-              <textarea id="message"></textarea>
-            </div>
-            <button type="submit">SUBMIT</button>
-          </form>
+              <div className='Normal'>
+                <label htmlFor="phone">Phone Number</label>
+                <div className='phoneInput'>
+                  <select style={{ width: '22%' }} id="code" type="tel">
+                    {countryCodes.map((country, index) => (
+                      <option key={index} value={country.code}>
+                        {country.flag} {country.code}
+                      </option>
+                    ))}
+                  </select>
+                  <input style={{ width: '78%' }} id="phone" type="tel" />
+                </div>
+              </div>
+              <div className='Normal'>
+                <label htmlFor="message">Message</label>
+                <textarea id="message"></textarea>
+              </div>
+              <button type="submit">SUBMIT</button>
+            </form>
+          </div>
         </div>
       </div>
     </>
